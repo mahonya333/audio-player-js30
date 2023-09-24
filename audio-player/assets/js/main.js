c800e6341abe225882ee090ffdebdd09c62bb594
Vladyslav Mahonia <mahonya333@gmail.com>
2023-09-24 15:10:52 +0300

    feat: added basic script

---
+++ audio-player/assets/js/main.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+    initialization();
+});
+
+function initialization() {
+    const audio = document.getElementById('audio');
+    const playBtn = document.querySelector('.player__play-btn');
+    const pauseBtn = document.querySelector('.player__pause-btn');
+    let isPlay = false;
+    
+    playBtn.addEventListener('click', playAudio);
+    pauseBtn.addEventListener('click', pauseAudio);
+
+    function playAudio() {
+        audio.currentTime = 0;
+        audio.play();
+    }
+    function pauseAudio() {
+        audio.pause();
+    }
+}
+
+
+
